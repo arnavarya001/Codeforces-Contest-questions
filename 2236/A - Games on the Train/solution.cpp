020fde9c6332fbcef68375832100fff796458dc2
Arnav Arya <arnavarya2004@gmail.com>
2026-07-23 20:10:06 +0530

    Add Games on the Train [A] from Codeforces

---
+++ 2236/A - Games on the Train/solution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        vector<int> h(n);
+        for (int i = 0; i < n; i++) {
+            cin >> h[i];
+        }
+        int mn = *min_element(h.begin(), h.end());
+        int mx = *max_element(h.begin(), h.end());
+ 
+        cout << mx - mn + 1 << endl;
+    }
+ 
+    return 0;
+}
